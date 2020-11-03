164384c6054f9ad6871f816b4540582ea737fa21
JOO200 <github@joo200.de>
2020-11-03 20:32:34 +0100

    Updated to WorldEdit 7.2.0

--- src/main/java/de/terraconia/backups/impl/WorldEditImpl.java
+++ src/main/java/de/terraconia/backups/impl/WorldEditImpl.java
@@ -6,59 +6,63 @@ import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.util.SideEffect;
+import com.sk89q.worldedit.util.SideEffectSet;
 import com.sk89q.worldedit.world.World;
 import de.terraconia.backups.CopyInterface;
-import de.terraconia.backups.tasks.SchematicTask;
 import de.terraconia.backups.manager.BackupManager;
+import de.terraconia.backups.tasks.SchematicTask;
 import org.bukkit.entity.Player;
 
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class WorldEditImpl implements CopyInterface {
     @Override
     public CompletableFuture<Boolean> copySchematic(SchematicTask task) {
-        EditSession session = WorldEdit.getInstance().getEditSessionFactory().getEditSession(task.getTarget(),
-                BackupManager.maxWorldEditBlockAmount);
-        session.setFastMode(true);
-        session.setReorderMode(EditSession.ReorderMode.FAST);
-        if(task.getBlockBag() != null) session.setBlockBag(task.getBlockBag());
-        if(task.getMask() != null) session.setMask(task.getMask());
+        EditSession session = WorldEdit.getInstance().newEditSessionBuilder().world(task.getTarget()).
+            maxBlocks(BackupManager.maxWorldEditBlockAmount).build();
+        try (session) {
+            session.setSideEffectApplier(SideEffectSet.none().with(SideEffect.LIGHTING, SideEffect.State.ON));
+            session.setReorderMode(EditSession.ReorderMode.FAST);
+            if (task.getBlockBag() != null) session.setBlockBag(task.getBlockBag());
+            if (task.getMask() != null) session.setMask(task.getMask());
 
-        ClipboardHolder holder = new ClipboardHolder(task.getClipboard());
-        if(task.getTransform() != null) holder.setTransform(task.getTransform());
+            ClipboardHolder holder = new ClipboardHolder(task.getClipboard());
+            if (task.getTransform() != null) holder.setTransform(task.getTransform());
 
-        Operation operation = holder.createPaste(session)
-                .to(task.getOrigin())
-                .ignoreAirBlocks(task.isIgnoreAir())
-                .build();
-        try {
-            Operations.complete(operation);
-        } catch (WorldEditException e) {
-            return CompletableFuture.failedFuture(e);
+            Operation operation = holder.createPaste(session)
+                    .to(task.getOrigin())
+                    .ignoreAirBlocks(task.isIgnoreAir())
+                    .build();
+            try {
+                Operations.complete(operation);
+            } catch (WorldEditException e) {
+                return CompletableFuture.failedFuture(e);
+            }
+        } finally {
+            if(task.getBlockBag() != null) task.getBlockBag().flushChanges();
+            task.setMissingBlocks(session.popMissingBlocks());
         }
-        session.flushSession();
-        if(session.getBlockBag() != null) session.getBlockBag().flushChanges();
-        task.setMissingBlocks(session.popMissingBlocks());
         return CompletableFuture.completedFuture(true);
     }
 
     @Override
     public CompletableFuture<Boolean> pasteRegion(Player player, ClipboardHolder clipboard, World world, String tag) {
-        EditSession session = WorldEdit.getInstance().getEditSessionFactory().getEditSession(world,
-                BackupManager.maxWorldEditBlockAmount);
-        session.setFastMode(true);
-        session.setReorderMode(EditSession.ReorderMode.FAST);
+        try (EditSession session = WorldEdit.getInstance().newEditSessionBuilder().world(world).
+                maxBlocks(BackupManager.maxWorldEditBlockAmount).build()) {
+            session.setSideEffectApplier(SideEffectSet.none().with(SideEffect.LIGHTING, SideEffect.State.ON));
+            session.setReorderMode(EditSession.ReorderMode.FAST);
 
-        Operation operation = clipboard.createPaste(session)
-                .ignoreAirBlocks(false)
-                .build();
-        try {
-            Operations.complete(operation);
-        } catch (WorldEditException e) {
-            return CompletableFuture.failedFuture(e);
+            Operation operation = clipboard.createPaste(session)
+                    .ignoreAirBlocks(false)
+                    .build();
+            try {
+                Operations.complete(operation);
+            } catch (WorldEditException e) {
+                return CompletableFuture.failedFuture(e);
+            }
         }
-        session.flushSession();
         return CompletableFuture.completedFuture(true);
+
     }
 }
